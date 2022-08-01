887df6a78270fbdd2387f811e649eac42d64b41e
Jodu555 <Jodu505@gmail.com>
2022-08-01 16:34:15 +0200

    Moved last infos under the stats

--- client/src/components/ItemCard.vue
+++ client/src/components/ItemCard.vue
@@ -22,6 +22,14 @@
 		<img v-if="value.imageurl" :src="value.imageurl" class="card-img-top py-2" alt="previewImage" />
 		<div class="card-body">
 			<h1 class="card-title text-center">{{ value.name }}</h1>
+		</div>
+		<ul v-if="value.stats != null" class="list-group list-group-flush">
+			<li class="list-group-item"><b>Dauer:</b> {{ toNiceTime(value.stats.length) }}</li>
+			<li class="list-group-item"><b>Größe:</b> {{ toNiceSize(value.stats.size) }}</li>
+			<li class="list-group-item"><b>Geschwindigkeit:</b> {{ toNiceSize(value.stats.speed) }}/s</li>
+			<li class="list-group-item"><b>Status:</b> {{ statusInfo }}</li>
+		</ul>
+		<div class="card-body">
 			<div class="row justify-content-around">
 				<span v-if="value.lastStats" class="col-auto text-muted"
 					>Letzte Statistiken: {{ new Date(value.lastStats).toLocaleTimeString() }}</span
@@ -30,15 +38,7 @@
 					>Letztes Bild: {{ new Date(value.lastImage).toLocaleTimeString() }}</span
 				>
 			</div>
-		</div>
-		<ul v-if="value.stats != null" class="list-group list-group-flush">
-			<li class="list-group-item"><b>Dauer:</b> {{ toNiceTime(value.stats.length) }}</li>
-			<li class="list-group-item"><b>Größe:</b> {{ toNiceSize(value.stats.size) }}</li>
-			<li class="list-group-item"><b>Geschwindigkeit:</b> {{ toNiceSize(value.stats.speed) }}/s</li>
-			<li class="list-group-item"><b>Status:</b> {{statusInfo}}</li>
-		</ul>
-		<div class="card-body">
-			<div class="row justify-content-around">
+			<div class="row justify-content-around py-2">
 				<a
 					:href="`https://twitch.tv/${value.name}`"
 					target="_blank"
