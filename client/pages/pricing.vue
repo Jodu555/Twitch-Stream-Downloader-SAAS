e3e97bf560edbeb7d6a2290fc55583b2b081ad83
Jodu555 <Jodu505@gmail.com>
2025-03-29 03:06:26 +0100

    Some Updates to the pricing

--- client/pages/pricing.vue
+++ client/pages/pricing.vue
@@ -34,27 +34,27 @@
                 <a href="/" class="d-flex align-items-center link-body-emphasis text-decoration-none">
                     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="32" class="me-2" viewBox="0 0 118 94"
                         role="img">
-                        <title>Bootstrap</title>
+                        <title>Twitch Stream Recorder</title>
                         <path fill-rule="evenodd" clip-rule="evenodd"
                             d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z"
                             fill="currentColor"></path>
                     </svg>
-                    <span class="fs-4">Pricing example</span>
+                    <span class="fs-4">Twitch Stream Recorder</span>
                 </a>
 
                 <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
-                    <a class="me-3 py-2 link-body-emphasis text-decoration-none" href="#">Features</a>
-                    <a class="me-3 py-2 link-body-emphasis text-decoration-none" href="#">Enterprise</a>
-                    <a class="me-3 py-2 link-body-emphasis text-decoration-none" href="#">Support</a>
-                    <a class="py-2 link-body-emphasis text-decoration-none" href="#">Pricing</a>
+                    <a class="me-3 py-2 link-body-emphasis text-decoration-none" href="#">Home</a>
+                    <a class="me-3 py-2 link-body-emphasis text-decoration-none" href="#">Pricing</a>
+                    <a class="me-3 py-2 link-body-emphasis text-decoration-none" href="#">FAQ</a>
                 </nav>
             </div>
 
             <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
                 <h1 class="display-4 fw-normal text-body-emphasis">Pricing</h1>
-                <p class="fs-5 text-body-secondary">Quickly build an effective pricing table for your potential
-                    customers with this Bootstrap example. It’s built with default Bootstrap components and utilities
-                    with little customization.</p>
+                <p class="fs-5 text-body-secondary">Quickly record and download Streams at the best possible Quality
+                    completely Ad-Free and without any limits.
+                    No muted or disabled VOD's! Just Record and Download!
+                </p>
             </div>
         </header>
 
@@ -96,7 +96,13 @@
                             }}
                             </th>
                             <td v-for="value in Object.keys(pricingTable)" :key="value">
-                                <template v-if="value != 'free'">
+                                <template
+                                    v-if="pricingTable[value as PricingTableKey][key as keyof PricingTableObject] == true && pricingTable[value as PricingTableKey][key as keyof PricingTableObject] !== 1">
+                                    <svg class="bi" width="24" height="24">
+                                        <use xlink:href="#check" />
+                                    </svg>
+                                </template>
+                                <template v-else-if="value != 'free'">
                                     <b>{{ pricingTable[value as PricingTableKey][key as keyof PricingTableObject] }}</b>
                                 </template>
                                 <template v-else>
@@ -119,6 +125,7 @@
 <script lang="ts" setup>
 
 interface PricingTableObject {
+    adFree: boolean;
     simulRecordings: number;
     staleVideos: number;
     maxRecordingTime: number;
@@ -143,6 +150,8 @@ function keyToNiceName(key: PricingTableKey) {
 
 function limitationToNiceName(key: keyof PricingTableObject) {
     switch (key) {
+        case 'adFree':
+            return 'Ad-Free';
         case 'simulRecordings':
             return 'Simul. Recordings';
         case 'staleVideos':
@@ -165,6 +174,7 @@ const cardTable = ref<CardTable>({
     free: {
         price: 0,
         features: [
+            'Completely Ad-Free',
             '1 Simultaneous Stream Recording',
             '2 Recorded Streams',
             '8 Hours Recording Time',
@@ -175,6 +185,7 @@ const cardTable = ref<CardTable>({
     premium: {
         price: 10,
         features: [
+            'Completely Ad-Free',
             '5 Simultaneous Stream Recordings',
             '7 Recorded Streams',
             '24 Hours Recording Time',
@@ -183,8 +194,9 @@ const cardTable = ref<CardTable>({
         ],
     },
     ultimate: {
-        price: 23,
+        price: 25,
         features: [
+            'Completely Ad-Free',
             '8 Simultaneous Stream Recordings',
             '20 Recorded Streams',
             '24 Hours Recording Time',
@@ -196,6 +208,7 @@ const cardTable = ref<CardTable>({
 
 const pricingTable = ref<PricingTable>({
     free: {
+        adFree: true,
         simulRecordings: 1,
         staleVideos: 2,
         maxRecordingTime: 8,
@@ -203,6 +216,7 @@ const pricingTable = ref<PricingTable>({
         videoRetentionDays: 2,
     },
     premium: {
+        adFree: true,
         simulRecordings: 5,
         staleVideos: 7,
         maxRecordingTime: 24,
@@ -210,6 +224,7 @@ const pricingTable = ref<PricingTable>({
         videoRetentionDays: 7,
     },
     ultimate: {
+        adFree: true,
         simulRecordings: 8,
         staleVideos: 20,
         maxRecordingTime: 24,
